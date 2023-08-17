4b8a4514fcb1e6729740739618b1986e5a70b74a
TanKhiem2001 <trantankhiem2001@gmail.com>
2023-08-17 12:26:16 +0700

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  //khiemcute
 }
 
 void loop() {
@@ -15,4 +16,8 @@ void loop() {
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+}
+void thich()
+{
+  /////////////
 }
